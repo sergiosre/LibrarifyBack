4301947f49e130c0a34942e68e633832f60475b9
Sergio <sergioromero.sre@gmail.com>
2020-11-19 14:54:30 +0100

    Añadido logger monolog y viendo el uso de los servicios y su modo de usarlos en controladores o en otros servicios

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -2,18 +2,28 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LibraryController extends AbstractController
 {
+    // private $logger;
+
+    // public function __construct(LoggerInterface $logger)
+    // {
+    //     $this->logger = $logger;
+    // }
 
     /**
      * @Route("/library/list", name="library_list")
      */
-    public function list()
-    { 
+    public function list(Request $request, LoggerInterface $logger)
+    {
+        $title = $request->get('title');
+        $logger->info('List action called');
         $response = new JsonResponse();
         $response->setData([
             'success' => true,
@@ -25,6 +35,10 @@ class LibraryController extends AbstractController
                 [
                     'id' => 1,
                     'title' => 'El Señor de los Anillos'
+                ],
+                [
+                    'id' => 3,
+                    'title' => $title
                 ]
             ]
         ]);
