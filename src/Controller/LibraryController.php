3643edf46f6e877a588c6696b775fb8143faaaf5
Sergio <sergioromero.sre@gmail.com>
2020-11-19 15:45:17 +0100

    Inlcuyendo doctrine y probando a guardar y traer datos de bbdd

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
+use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -18,28 +21,57 @@ class LibraryController extends AbstractController
     // }
 
     /**
-     * @Route("/library/list", name="library_list")
+     * @Route("/books", name="book_get")
      */
-    public function list(Request $request, LoggerInterface $logger)
+    public function list(Request $request, BookRepository $bookRepository)
     {
         $title = $request->get('title');
-        $logger->info('List action called');
+        $books = $bookRepository->findAll();
+        $booksAsArray  = [];
+        foreach ($books as $book) {
+            $booksAsArray[] = [
+                'id' => $book->getId(),
+                'title' => $book->getTitle(),
+                'image' => $book->getImage()
+            ];
+        }
         $response = new JsonResponse();
+        $response->setData([
+            'success' => true,
+            'data' => $booksAsArray
+        ]);
+
+        return $response;
+    }
+
+    /**
+     * @Route("/book/create", name="create_book")
+     */
+    public function createBook(Request $request, EntityManagerInterface $entityManager)
+    {
+        $book = new Book();
+        $response = new JsonResponse();
+
+        $title = $request->get('title', null);
+        if (empty($title)) {
+            $response->setData([
+                'success' => false,
+                'error' => 'Title cannot be empty',
+                'data' => null
+            ]);
+
+            return $response;
+        }
+        $book->setTitle($title);
+        $entityManager->persist($book);
+        $entityManager->flush();
         $response->setData([
             'success' => true,
             'data' => [
                 [
-                    'id' => 1,
-                    'title' => 'Harry Potter'
+                    'id' => $book->getId(),
+                    'title' => $book->getTitle()
                 ],
-                [
-                    'id' => 1,
-                    'title' => 'El Señor de los Anillos'
-                ],
-                [
-                    'id' => 3,
-                    'title' => $title
-                ]
             ]
         ]);
 
